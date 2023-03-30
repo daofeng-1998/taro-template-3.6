ea4c45bd6afce9a12b9e61eaa39f30033832d3e1
刀锋 <2080969361@qq.com>
2023-03-30 12:03:07 +0800

    feat: 增加cacheResultAsync工具函数

--- src/utils/AsyncTool.ts
+++ src/utils/AsyncTool.ts
@@ -1,3 +1,5 @@
+import { isUndefined } from './TypeTools/TypesTools';
+
 interface APIOption<R, ER,> {
     success?: (r: R) => void
     fail?: (r: ER) => void
@@ -37,8 +39,8 @@ export function promisify<P extends object, R, ER,>(api: (option?: P & APIOption
 /**
  * 异步循环call
  * */
-export function cycleCallAsync(count: number, method: Function): Function {
-    return async function (...args) {
+export function cycleCallAsync<P extends any[], R,>(count: number, method: (...args: P) => Promise<R>) {
+    return async function (...args: P) {
         let counter = 0;
         do {
             const [err, res] = await to(method(...args));
@@ -46,8 +48,44 @@ export function cycleCallAsync(count: number, method: Function): Function {
                 if (++counter >= count)
                     throw err;
             } else {
-                return res;
+                return res as R;
             }
         } while (true);
     };
+}
+
+/**
+ * 缓存异步结果
+ * @param api
+ * @returns
+ */
+export function cacheResultAsync<P extends any[], R,>(api: (...args: P) => Promise<R>) {
+    let result: R | undefined; // 将undefined视为无缓存结果
+    const taskList: Promise<any>[] = [];
+
+    async function task(...args: P): Promise<R> {
+        if (!isUndefined(result))
+            return result;
+
+        if (taskList.length > 0) {
+            await Promise.all(taskList);
+            taskList.splice(0);
+            return (result ?? null)!;
+        }
+
+        taskList.push(api(...args).then(res => result = res));
+
+        return await task(...args);
+    }
+    /**
+     * 不调用缓存结果
+     */
+    task.AsNoCache = api;
+
+    /**
+     * 清除缓存
+     */
+    task.clear = () => result = undefined;
+
+    return task;
 }
